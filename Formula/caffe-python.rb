23f253c4edf669232f0fe7e4d0fcaf366fa0be04
Jakob Dam Jensen <jakob@nopointlessapps.com>
2019-09-23 21:08:04 +0200

    Python caffe test

---
+++ Formula/caffe-python.rb
@@ -0,0 +1,71 @@
+class Caffe < Formula
+  desc "Fast open framework for deep learning"
+  homepage "https://caffe.berkeleyvision.org/"
+  url "https://github.com/BVLC/caffe/archive/1.0.tar.gz"
+  sha256 "71d3c9eb8a183150f965a465824d01fe82826c22505f7aa314f700ace03fa77f"
+  revision 14
+
+  bottle do
+    sha256 "e42c7b0dd7165199c299008551f1224353982421bb9fc3ef2a006c380907a6de" => :mojave
+    sha256 "5cb7b806c64970fe3013ba3e9c7c10ff0667bf5d2b90da03bde23e5d42461c69" => :high_sierra
+    sha256 "3186e66f6bead0992c8fbe39d4177c14b4e4328c2bb2cc22fc4d9e5113e27063" => :sierra
+  end
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "hdf5"
+  depends_on "leveldb"
+  depends_on "lmdb"
+  depends_on "opencv"
+  depends_on "protobuf"
+  depends_on "snappy"
+  depends_on "szip"
+
+  resource "test_model_weights" do
+    url "http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel"
+    sha256 "472d4a06035497b180636d8a82667129960371375bd10fcb6df5c6c7631f25e0"
+  end
+
+  # Fix compilation with OpenCV 4
+  # https://github.com/BVLC/caffe/issues/6652
+  patch do
+    url "https://github.com/BVLC/caffe/pull/6638.diff?full_index=1"
+    sha256 "6a6368d715284fabfa96660b6d24d1f4f419f3e6cdddab9a7293954fee4ec2bc"
+  end
+
+  def install
+    ENV.cxx11
+
+    args = std_cmake_args + %w[
+      -DALLOW_LMDB_NOLOCK=OFF
+      -DBUILD_SHARED_LIBS=ON
+      -DBUILD_docs=OFF
+      -DBUILD_matlab=OFF
+      -DBUILD_python=OFF
+      -DBUILD_python_layer=OFF
+      -DCPU_ONLY=ON
+      -DUSE_LEVELDB=ON
+      -DUSE_LMDB=ON
+      -DUSE_NCCL=OFF
+      -DUSE_OPENCV=ON
+      -DUSE_OPENMP=OFF
+    ]
+
+    system "cmake", ".", *args
+    system "make", "install"
+    pkgshare.install "models"
+  end
+
+  test do
+    model = "bvlc_reference_caffenet"
+    m_path = "#{pkgshare}/models/#{model}"
+    resource("test_model_weights").stage do
+      system "#{bin}/caffe", "test",
+             "-model", "#{m_path}/deploy.prototxt",
+             "-solver", "#{m_path}/solver.prototxt",
+             "-weights", "#{model}.caffemodel"
+    end
+  end
+end
